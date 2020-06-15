bd7dea4f7084ae2589d03cf191409cb2ff7395e3
Marius Ackerman <goccmack@gmail.com>
2020-06-15 17:55:59 +0200

    Bug fixed

--- md/md.go
+++ md/md.go
@@ -19,8 +19,6 @@ package md
 
 import (
 	"io/ioutil"
-	"strings"
-	"unicode"
 )
 
 var ch rune
@@ -33,68 +31,31 @@ func GetSource(mdfile string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	in, out := strings.NewReader(string(inbuf)), new(strings.Builder)
-	ch = next(in)
-	for in.Len() > 0 {
-		switch ch {
-		case '\u0060':
-			out.WriteString(space(ch))
-			ch = next(in)
-			if ch == '\u0060' {
-				out.WriteString(space(ch))
-				ch = next(in)
-				if ch == '\u0060' {
-					out.WriteString(space(ch))
-					writeSpec(in, out)
-					ch = next(in)
+	input := []rune(string(inbuf))
+	loadMd(input)
+	return string(input), nil
+}
+
+func loadMd(input []rune) {
+	i := 0
+	text := true
+	for i < len(input) {
+		if i <= len(input)-3 && input[i] == '`' && input[i+1] == '`' && input[i+2] == '`' {
+			text = !text
+			for j := 0; j < 3; j++ {
+				input[i+j] = ' '
+			}
+			i += 3
+		}
+		if i < len(input) {
+			if text {
+				if input[i] == '\n' {
+					input[i] = '\n'
+				} else {
+					input[i] = ' '
 				}
 			}
-		default:
-			out.WriteString(space(ch))
-			ch = next(in)
+			i += 1
 		}
 	}
-	return out.String(), nil
-}
-
-func space(ch rune) string {
-	if unicode.IsSpace(ch) {
-		return string(ch)
-	}
-	return " "
-}
-
-func writeSpec(in *strings.Reader, out *strings.Builder) {
-	ch = next(in)
-	for in.Len() > 0 {
-		switch {
-		case ch == '\u0060':
-			ch = next(in)
-			if ch == '\u0060' {
-				ch = next(in)
-				if ch == '\u0060' {
-					out.WriteString("   ")
-					return
-				}
-				out.WriteString("\u0060\u0060")
-			} else {
-				out.WriteString("\u0060")
-			}
-			return
-		default:
-			out.WriteRune(ch)
-			ch = next(in)
-		}
-	}
-}
-
-func next(in *strings.Reader) rune {
-	if in.Len() <= 0 {
-		return -1
-	}
-	ch, _, err := in.ReadRune()
-	if err != nil {
-		panic(err)
-	}
-	return ch
 }
